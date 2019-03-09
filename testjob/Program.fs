5bf5a16a8ea8cd8d535c6d4a8ef87e1d6d5a460e
Lee McSorley <=>
2019-03-09 16:49:54 +0000

    file output

--- testjob/Program.fs
+++ testjob/Program.fs
@@ -1,8 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
 
 [<EntryPoint>]
 let main argv =
     printfn "Paperspace TEST"
+    File.WriteAllLines("artifacts/file.txt", [|"hello"; "world"; "paperspace"|])
     0 // return an integer exit code
